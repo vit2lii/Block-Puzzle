58eb51d2c6aeb89b0e81bba8f48ad9e56ba00cf0
Vitalii Romaniuk <vitalii.romaniuk.2@student.tuke.sk>
2024-02-23 21:55:54 +0100

    Add project structure

---
+++ src/main/java/com/example/BlockPuzzle/core/BlockPuzzleApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlockPuzzle.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockPuzzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockPuzzleApplication.class, args);
+	}
+
+}
